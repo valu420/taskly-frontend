f00b70096eac9b0fc003f80b59c2448724f158e0
Valu <valentina.sanchez.castrillon@correounivalle.edu.co>
2025-09-09 22:50:46 -0500

    changes in index

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+// Configuración para múltiples páginas
+export default defineConfig({
+  root: ".", // raíz del proyecto
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        home: "src/pages/home.html",
+        dashboard: "src/pages/dashboard.html",
+        addTask: "src/pages/add-task.html",
+        kanban: "src/pages/kanban.html",
+        login: "src/pages/login.html",
+        register: "src/pages/register.html",
+        recoverPassword: "src/pages/recoverPassword.html",
+        confirmPassword: "src/pages/confirmPassword.html"
+      }
+    }
+  }
+})
